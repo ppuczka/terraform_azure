5eb66fe9f5e12c31801d4a712424c0b320ff2e57
p.puczka <p.puczka@gmail.com>
2020-04-03 23:12:57 +0200

    provisioning azure working

--- ex_2/main.tf
+++ ex_2/main.tf
@@ -1,104 +1,102 @@
+
+# Configure the Microsoft Azure Provider.
 provider "azurerm" {
-    version = "~>1.44.0"
-
-    subscription_id = "0ad5ac4a-b031-42a2-9fe5-5feb754f1822"   
+version = "~>1.44.0"
+subscription_id = "0ad5ac4a-b031-42a2-9fe5-5feb754f1822"   
+}
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "myTFResourceGroup"
+    location = "westeurope"
+}
+# Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westeurope"
+    resource_group_name = "azurerm_resource_group.rg.name"
 }
 
-resource "azurerm_resource_group" "rg_tutorial_1" {
-    name = "tutorial_1"
-    location = "North Europe"
-
-    tags = {
-        Environment = "Terraform Getting Started"
-        Team = "DevOps_Workshop"
-    }
-}
-
-resource "azurerm_virtual_network" "vnet_tutorial" {
-    name            = "myTutorialVnet"
-    address_space   = ["10.0.0.0./16"]
-    location        = "North Europe"
-    resource_group_name = azurerm_resource_group.rg_tutorial_1.name
-}
-
+# Create subnet
 resource "azurerm_subnet" "subnet" {
-    name                    = "myTutorialSubnet"
-    resource_group_name     = azurerm_resource_group.rg_tutorial_1.name
-    virtual_network_name    = azurerm_virtual_network.vnet_tutorial.name
-    address_prefix          = "10.0.1.0/24"  
-}   
+    name                 = "myTFSubnet"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefix       = "10.0.1.0/24"
+}
 
+# Create public IP
 resource "azurerm_public_ip" "publicip" {
-    name                = "${var.resource_prefix}TFPublicIP"
-    location            = var.location
-    resource_group_name = azurerm_resource_group.rg_tutorial_1.name
-    allocation_method   = "Static"
-} 
+    name                         = "myTFPublicIP"
+    location                     = "westeurope"
+    resource_group_name          = azurerm_resource_group.rg.name
+    public_ip_address_allocation = "dynamic"
+}
 
+# Create Network Security Group and rule
 resource "azurerm_network_security_group" "nsg" {
-  name                = "myTFNSG"
-  location            = "North Europe"
-  resource_group_name = azurerm_resource_group.rg_tutorial_1.name
+    name                = "myTFNSG"
+    location            = "westeurope"
+    resource_group_name = azurerm_resource_group.rg.name
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 }
 
 # Create network interface
 resource "azurerm_network_interface" "nic" {
-  name                      = "myNIC"
-  location                  = "North Europe"
-  resource_group_name       = azurerm_resource_group.rg_tutorial_1.name
-  network_security_group_id = azurerm_network_security_group.nsg.id
+    name                      = "myNIC"
+    location                  = "westeurope"
+    resource_group_name       = azurerm_resource_group.rg.name
+    network_security_group_id = azurerm_network_security_group.nsg.id
 
-  ip_configuration {
-    name                          = "myNICConfg"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip.id
-  }
+    ip_configuration {
+        name                          = "myNICConfg"
+        subnet_id                     = azurerm_subnet.subnet.id
+        private_ip_address_allocation = "dynamic"
+        public_ip_address_id          = azurerm_public_ip.publicip.id
+    }
 }
 
 # Create a Linux virtual machine
 resource "azurerm_virtual_machine" "vm" {
-  name                  = "myTFVM"
-  location              = "North Europe"
-  resource_group_name   = azurerm_resource_group.rg_tutorial_1.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_DS1_v2"
+    name                  = "myTFVM"
+    location              = "westeurope"
+    resource_group_name   = azurerm_resource_group.rg.name
+    network_interface_ids = [azurerm_network_interface.nic.id]
+    vm_size              = "Standard_F2s_v2"
 
-  storage_os_disk {
-    name              = "myOsDisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
-  }
+    storage_os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Standard_LRS"
+    }
 
+    storage_image_reference {
+        publisher = "OpenLogic"
+        offer     = "CentOS"
+        sku       = "7.5"
+        version   = "latest"
+    }
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04.0-LTS"
-    version   = "latest"
-  }
+    os_profile {
+        computer_name  = "myTFVM"
+        admin_username = "plankton"
+        admin_password = "Password1234!"
+    }
 
-  os_profile {
-    computer_name  = "myTFVM"
-    admin_username = "plankton"
-    admin_password = "Password1234!"
-  }
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-
-  }
 }
