eab6e5342b3edf10971277817b8e9c04360d4cbb
Przemyslaw Puczka <p.puczka@gmail.com>
2020-08-17 22:46:53 +0200

    opened 8080 port for jenkins

--- terraform_ra_azure/azure_terraform_lab/lab4/jenkins.tf
+++ terraform_ra_azure/azure_terraform_lab/lab4/jenkins.tf
@@ -50,22 +50,40 @@ resource "azurerm_network_security_group" "myterraformnsg" {
   location            = var.location
   resource_group_name = azurerm_resource_group.myterraformgroup.name
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
   tags = {
     environment = var.environment
   }
 }
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                       = "SSH"
+  priority                   = 1001
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "22"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+  network_security_group_name = azurerm_network_security_group.myterraformnsg.name
+}
+
+resource "azurerm_network_security_rule" "jenkins" {
+  name                       = "jenkins"
+  priority                   = 1100
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "8080"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+  network_security_group_name = azurerm_network_security_group.myterraformnsg.name
+}
+
+
 
 # Create network interface
 resource "azurerm_network_interface" "myterraformnic" {
